f85e92349294810e5f72123c8779d95e2358e49d
Sem <931684+sembrestels@users.noreply.github.com>
2025-09-02 05:05:23 +0200

    build(vite): fix SSR build when running with Deno - Add "@wagmi/core@^2.20.3" to dependencies and update bun.lock accordingly - Ignore non-Bun lockfiles in .gitignore (npm/yarn/pnpm/deno)

--- vite.config.ts
+++ vite.config.ts
@@ -19,4 +19,20 @@ export default defineConfig({
       exclude: ["node_modules", "test", "build"],
     },
   },
+  ...denoWorkaround(),
 });
+
+function denoWorkaround() {
+  const isDeno = typeof globalThis !== "undefined" && "Deno" in globalThis;
+  if (!isDeno) {
+    return undefined;
+  }
+  // See: https://github.com/remix-run/react-router/issues/12568#issuecomment-2625776697
+  return {
+    resolve: {
+      alias: {
+        'react-dom/server': 'react-dom/server.node',
+      },
+    }
+  };
+}
